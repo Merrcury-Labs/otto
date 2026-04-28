43be7ed8547b73b7e76c01c00911e50c949bd4c5
Z-khumalo <z.khumalo0978@gmail.com>
2026-04-28 20:42:01 +0200

    feat: Enhance authentication configuration with better-auth integration

--- apps/web/lib/auth.ts
+++ apps/web/lib/auth.ts
@@ -1,8 +1,15 @@
 import { betterAuth } from "better-auth";
+import { connection } from "next/server";
 import { Pool } from "pg";
 
 export const auth = betterAuth({
+    baseURL: process.env.BETTER_AUTH_URL || "http://localhost:3000",
+    emailAndPassword: {
+        enabled: true,
+    },
+    secret: process.env.BETTER_AUTH_SECRET,
     database: new Pool({
-        // connection options
+        connectionString: process.env.connection_string,
+        ssl: process.env.NODE_ENV === "production" ? { rejectUnauthorized: false } : false,
     }),
 })
